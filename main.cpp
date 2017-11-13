c5946ceb4e7345119ba70c14f07cec61aee16de7
prizrak8990 <32384718+prizrak8990@users.noreply.github.com>
2017-11-13 13:15:40 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "header1.h"
+#include "header2.h"
+using namespace std;
+using namespace toy;
+using namespace joy;
+
+int main()
+{
+using toy::foo;
+cout << k << endl << l << endl;
+foo();
+return 0;
+}
